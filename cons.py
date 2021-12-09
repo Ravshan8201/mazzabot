16faffb519d61878aa0b8477a6518b25726fe866
Ravshan8201 <88588235+Ravshan8201@users.noreply.github.com>
2021-12-09 22:37:34 +0500

    Create cons.py

---
+++ cons.py
@@ -0,0 +1,62 @@
+TOKE = '2079008584:AAG9atSPHCS_i9OM05Um1sMgQq1AWeDM0AM'
+
+
+dct =  {1:['Начать',#0
+             'Нажмите на кнопку',#1
+             '🎫Введите промокод:',#2
+             'Язык🇷🇺🇺🇿',#3
+              'Номер телефона☎️',#4
+              '{}, Вы уже есть в нашей базе, нам не хватает вашего номера(\nСкиньте ваш контакт 😘', #5
+              'Поделиться контактом☎', #6
+              'Подилитесь контактом☎', #7
+              'Выберите:', #8
+              '⬅️Назад', #9
+              ' К сожилению в этот раз Вам не повезло🥲, в следуюший раз Вам обязательно повезёт🙂🙃. Продолжайте участвовать в Промоакции. Пусть удача будет на Вашей стороне😇😇😇', #10
+              '✅Готово', #11
+              'Пожалуйста, ждите, Мы скоро с Вами свяжемся⏳...', #12
+              '🏘Введите Ваше место проживание:', #13
+              '👤Пожалуйста, введите Ваше имя:', #14
+              '🤩🥳😇Поздравляем!!!\n Вы выиграли {}😍😍😍\nПожалуйста, оставьте свои данные', #15
+              'Привет, Мы очень рады за Ваше участие в нашем промоакции Маззами Сизларга Маззами. Пожалуйста введите Промокод🎫:', #16
+              '‼️‼️‼️‼️‼️\n\nХраните в целости и сохранности пачку и карточку с промокодом\n❗️ ВНИМАНИЕ! НЕ ОТКЛЕИВАТЬ СТИКЕР ОТ УПАКОВКИ ❗️\n\n‼️‼️‼️‼️‼️',  # 17
+
+           ],
+
+       2: ['Boshlash',  # 0
+            'Asosiy menyu tugmasini bosing',  # 1
+            '🎫Promo-kodni tering:',# 2
+            'Til🇺🇿🇷🇺',  # 3
+            'Telefon nomer☎️',  # 4
+
+            '{}, Siz Bizning malumotlar bazamizda mavjudsiz, Bizga Sizning telefon raqamingiz kerak xolos (\nKontakt ulashihtugmasini bosing 😘',  # 5
+            'Kontaktni ulashish☎️',  # 6
+            'Kontaktingizni ulashing☎️', # 7
+            'Tanlang:',  # 8
+            '⬅️Orqaga',  # 9
+            '''Afsus bu safar Sizga omad kulib boqmadi🥲, kelasi safar Albatta omadingiz keladi🙂🙃. Mahsulotlarni harid qilib Promo-aksiyada O'z imkoniyatingizni yana bir bor sinab ko'ring. Omad yor bo'lsin😇😇😇''',
+            # 10
+            '✅Tayyor',  # 11
+            'Iltimos biroz sabr qiling tez orada Biz Siz bilan bog’lanamiz⏳...',  # 12
+            '🏘Iltimos, yashash manzilingizni kiriting:',  # 13
+            '👤Iltimos, ismingizni kiriting:',  # 14
+            '''🤩🥳😇Tabriklaymiz!!!\n\nSiz {} yutib oldingiz😍😍😍\niltimos ma'lumotlaringizni qoldiring''',  # 15
+            'Assalomu alaykum, Mazzami Sizlarga Mazzami promo-aksiyasida ishtirok etayotganingizdan mamnunmiz. Iltimos Promo-kodni kiriting🎫:',  # 16
+            '''‼️‼️‼️‼️‼️\n\nPromo-kod va qadoqni saqlab qo'ying\n❗️ DIQQAT!PROMO-KOD STIKERINI QADOQDAN SHILMANG ❗️\n\n‼️‼️‼️‼️‼️''',# 17
+            ]}
+
+
+
+pdct  = {
+    'televizor':['qwerty','ytrewq'],
+    'telefon':['zxcvb','bvcxz'],
+}
+
+promoc = []
+promoca = []
+user_list =[]
+
+delete = []
+
+admindct = { 1:[1837175161, 957531477, 2071126215] }
+
+vvv = []
